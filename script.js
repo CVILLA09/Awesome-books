df23c98ae0dd8a0137a1064be05198d8b8ddde56
Cristian Villa <c.villavirgen@gmail.com>
2023-07-12 12:55:55 -0600

    Final commit

--- script.js
+++ script.js
@@ -1,82 +1,81 @@
-// Load books and idCounter from localStorage, or initialize them if they don't exist
-let books = JSON.parse(localStorage.getItem('books')) || [];
-let idCounter = JSON.parse(localStorage.getItem('idCounter')) || 0;
+/* eslint max-classes-per-file: ["error", 2] */
 
-// Function to add a book to the books array and update localStorage
-function addBook(title, author) {
-  const book = {
-    id: idCounter, // Unique id for each book
-    title,
-    author,
-  };
-
-  books.push(book); // Add the book to the books array
-  idCounter += 1; // Increment idCounter
-  // Update books and idCounter in localStorage
-  localStorage.setItem('books', JSON.stringify(books));
-  localStorage.setItem('idCounter', JSON.stringify(idCounter));
-}
-
-// Function to remove a book from the books array and update localStorage
-function removeBook(id) {
-  books = books.filter((book) => book.id !== id); // Remove the book with the given id
-  localStorage.setItem('books', JSON.stringify(books)); // Update books in localStorage
-}
-
-// Function to display the books on the page
-function displayBooks() {
-  const bookList = document.getElementById('book-list'); // Get the book list element
-  bookList.innerHTML = ''; // Clear the book list
-
-  // Loop over the books array and create elements for each book
-  for (let i = 0; i < books.length; i += 1) {
-    const bookDiv = document.createElement('div'); // Div to hold the book info
-
-    // Create and append elements for the book title and author
-    const titleElement = document.createElement('p');
-    titleElement.textContent = `Title: ${books[i].title}`;
-    bookDiv.appendChild(titleElement);
-
-    const authorElement = document.createElement('p');
-    authorElement.textContent = `Author: ${books[i].author}`;
-    bookDiv.appendChild(authorElement);
-
-    // Create and append a remove button for the book
-    const removeButton = document.createElement('button');
-    removeButton.textContent = 'Remove';
-    // Use an IIFE to create a new scope for each iteration of the loop
-    ((index, books) => {
-      removeButton.addEventListener('click', () => {
-        removeBook(books[index].id); // Remove the book when the button is clicked
-        displayBooks(); // Update the book list
-      });
-    })(i, books);
-    bookDiv.appendChild(removeButton);
-
-    // Create and append a horizontal line for separation
-    const hr = document.createElement('hr');
-    bookDiv.appendChild(hr);
-
-    bookList.appendChild(bookDiv); // Add the book div to the book list
+// Book class to represent individual books
+class Book {
+  constructor(id, title, author) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
   }
 }
+
+// BooksCollection class to manage the collection of books
+class BooksCollection {
+  constructor() {
+    this.books = JSON.parse(localStorage.getItem('books')) || [];
+    this.idCounter = JSON.parse(localStorage.getItem('idCounter')) || 0;
+  }
+
+  // Method to add a book to the books array and update localStorage
+  addBook(title, author) {
+    const book = new Book(this.idCounter, title, author);
+    this.books.push(book);
+    this.idCounter += 1;
+    localStorage.setItem('books', JSON.stringify(this.books));
+    localStorage.setItem('idCounter', JSON.stringify(this.idCounter));
+  }
+
+  // Method to remove a book from the books array and update localStorage
+  removeBook(id) {
+    this.books = this.books.filter((book) => book.id !== id);
+    localStorage.setItem('books', JSON.stringify(this.books));
+  }
+
+  // Method to display the books on the page
+  displayBooks() {
+    const bookList = document.getElementById('book-list');
+    bookList.innerHTML = '';
+
+    this.books.forEach((book, index) => {
+      const bookDiv = document.createElement('div');
+      const titleElement = document.createElement('p');
+      titleElement.textContent = `"${book.title}" by`;
+      bookDiv.appendChild(titleElement);
+
+      const authorElement = document.createElement('span');
+      authorElement.textContent = ` ${book.author}`; // Notice the space before ${book.author}
+      titleElement.appendChild(authorElement);
+
+      const removeButton = document.createElement('button');
+      removeButton.textContent = 'Remove';
+      removeButton.addEventListener('click', () => {
+        this.removeBook(book.id);
+        this.displayBooks();
+      });
+      bookDiv.appendChild(removeButton);
+
+      bookList.appendChild(bookDiv);
+    });
+  }
+}
+
+// Instantiate BooksCollection
+const myBooks = new BooksCollection();
 
 // Add an event listener to the form to add a book when the form is submitted
 const form = document.getElementById('book-form');
 form.addEventListener('submit', (event) => {
-  event.preventDefault(); // Prevent the form from being submitted
+  event.preventDefault();
 
-  // Get the values of the title and author input fields
   const title = document.getElementById('title').value;
   const author = document.getElementById('author').value;
 
-  addBook(title, author); // Add a new book
-  displayBooks(); // Update the book list
+  myBooks.addBook(title, author);
+  myBooks.displayBooks();
 
-  // Clear the input fields after adding a book
   document.getElementById('title').value = '';
   document.getElementById('author').value = '';
 });
 
 // Display the books when the page loads
-window.onload = displayBooks;
+window.onload = () => myBooks.displayBooks();
