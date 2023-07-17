7569b9960034e05d1fc36af00e9e1b68a2215f08
Cristian Villa <c.villavirgen@gmail.com>
2023-07-17 12:07:29 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,34 +6,32 @@ import { handleNavigation } from './navigation.js';
 // Declare booksCollection as an empty array
 let books = retrieveBooksFromStorage();
 
-document.addEventListener('DOMContentLoaded', () => {
-  // Add event listeners to the navigation links
-  const navLinks = document.querySelectorAll('.nav-link');
-  navLinks.forEach((link) => {
-    link.addEventListener('click', handleNavigation);
-  });
-
-  // Form submit event handler
-  const form = document.querySelector('.form');
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const titleInput = document.querySelector('.title-tag');
-    const authorInput = document.querySelector('.author-tag');
-
-    const title = titleInput.value;
-    const author = authorInput.value;
-
-    const book = new Book(title, author);
-    book.addBook(books);
-    saveBooksToStorage(books);
-
-    displayBooks(books);
-
-    titleInput.value = '';
-    authorInput.value = '';
-  });
-
-  // Initial display of books
-  displayBooks(books);
+// Add event listeners to the navigation links
+const navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach((link) => {
+  link.addEventListener('click', handleNavigation);
 });
+
+// Form submit event handler
+const form = document.querySelector('.form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const titleInput = document.querySelector('.title-tag');
+  const authorInput = document.querySelector('.author-tag');
+
+  const title = titleInput.value;
+  const author = authorInput.value;
+
+  const book = new Book(title, author);
+  book.addBook(books);
+  saveBooksToStorage(books);
+
+  displayBooks(books);
+
+  titleInput.value = '';
+  authorInput.value = '';
+});
+
+// Initial display of books
+displayBooks(books);
